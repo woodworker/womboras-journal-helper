9be00fbc81fda3047c69f84515d6c3f0694b3afc
Martin Holzhauer <martin@holzhauer.eu>
2021-10-15 17:35:39 +0200

    added roll request recognition in notes for dnd 5e system

--- src/hooks/renderJournalSheet.ts
+++ src/hooks/renderJournalSheet.ts
@@ -14,6 +14,111 @@ const renderTableToChatButton = (html: JQuery<HTMLElement>) => {
     });
 };
 
+const renderRollRequests = (html: JQuery<HTMLElement>): void => {
+    const skills = {
+        acr: ["ARC", "Acrobatics"],
+        ani: ["ANI", "AnimalHandling", "Animal Handling"],
+        arc: ["ARC", "Arcana"],
+        ath: ["ATH", "Athletics"],
+        dec: ["DEC", "Deception"],
+        his: ["HIS", "History"],
+        ins: ["INS", "Insight"],
+        itm: ["ITM", "Intimidation"],
+        inv: ["INV", "Investigation"],
+        med: ["MED", "Medicine"],
+        nat: ["NAT", "Nature"],
+        prc: ["PRC", "Perception"],
+        prf: ["PRF", "Performance"],
+        per: ["PER", "Persuasion"],
+        rel: ["REL", "Religion"],
+        slt: ["SLT", "SleightOfHand", "Sleight of Hand"],
+        ste: ["STE", "Stealth"],
+        sur: ["SUR", "Survival"],
+    };
+
+    const abilities = {
+        str: ["STR", "Strength"],
+        dex: ["DEX", "Dexterity"],
+        con: ["CON", "Constitution"],
+        int: ["INT", "Intelligence"],
+        wis: ["WIS", "Wisdom"],
+        cha: ["CHA", "Charisma"],
+    };
+
+    const concater = (items: { [key: string]: string[] }): string => {
+        return Object.values(items).reduce((prev: string, cur: string[]) => {
+            if (!prev) {
+                return cur.join("|");
+            }
+            return `${prev}|${cur.join("|")}`;
+        }, "");
+    };
+
+    const isInKeyedArray = (item: string, list: { [key: string]: string[] }): boolean => {
+        return Object.values(list).reduce((prev: boolean, cur: string[]) => {
+            if (cur.map((s) => s.toLocaleLowerCase()).includes(item.toLocaleLowerCase())) {
+                return true;
+            }
+            return prev;
+        }, false);
+    };
+
+    const getKey = (item: string, list: { [key: string]: string[] }): string | false => {
+        const result = Object.entries(list).filter(([_, listItemValues]) => {
+            return listItemValues.map((s) => s.toLocaleLowerCase()).includes(item.toLocaleLowerCase());
+        });
+
+        if (result.length === 1 && result[0].length === 2) {
+            return result[0][0];
+        }
+
+        return false;
+    };
+
+    const rollRequestRegexp = new RegExp(`(${concater({ ...skills, ...abilities })}) (save|check)`, "gi");
+
+    const textNodes = getTextNodes(html[0]);
+    replaceTextContent(textNodes, rollRequestRegexp, (fullMatch, skillOrAbility: string, rollType: string): HTMLElement => {
+        console.log({ fullMatch, skillOrAbility, rollType });
+
+        rollType = rollType.toUpperCase();
+
+        const isSkill = isInKeyedArray(skillOrAbility, skills);
+        const isAbility = isInKeyedArray(skillOrAbility, abilities);
+
+        // does not exist
+        if ((!isSkill && !isAbility) || (isSkill && rollType === "SAVE")) {
+            const span = document.createElement("span");
+            span.textContent = `${fullMatch}`;
+            return span;
+        }
+
+        const requestLink = document.createElement("a");
+        requestLink.href = "#";
+        requestLink.title = `Request a ${fullMatch} in Chat`;
+        requestLink.textContent = `${fullMatch}`;
+        requestLink.classList.add("entity-link");
+        requestLink.addEventListener("click", (ev) => {
+            ev.preventDefault();
+            ev.cancelBubble = true;
+            const key = getKey(skillOrAbility, { ...skills, ...abilities });
+            if (isSkill) {
+                ChatMessage.create({ content: `<h3>Roll request<h3><hr/>[[/roll 1d20+@skills.${key}.total]]{${fullMatch}}` });
+                return;
+            }
+            if (rollType === "SAVE") {
+                ChatMessage.create({ content: `<h3>Roll request<h3><hr/>[[/roll 1d20+@abilities.${key}.save]]{${fullMatch}}` });
+                return;
+            }
+
+            ChatMessage.create({ content: `<h3>Roll request<h3><hr/>[[/roll 1d20+@abilities.${key}.mod]]{${fullMatch}}` });
+            return;
+        });
+
+        return requestLink;
+    });
+};
+
 /**
  * entity data dsiplay
  */
@@ -23,37 +128,33 @@ const renderDocumentData = (html: JQuery<HTMLElement>): void => {
     const documentTypes = CONST.ENTITY_LINK_TYPES; //.concat("Compendium");
     const documentRegexp = new RegExp(`#(${documentTypes.join("|")})\\[([^\\]]+)\\](?:{([^}]+)})?`, "g");
 
-    replaceTextContent(
-        textNodes,
-        documentRegexp,
-        (_, type, target, datapath): HTMLElement => {
-            let document: Document|null = null;
-            if (CONST.ENTITY_TYPES.includes(type)) {
-                // Get the linked Entity
-                const collectionName = CONFIG[type].documentClass.collectionName;
-                const collection = game[collectionName];
-                document = /^[a-zA-Z0-9]{16}$/.test(target) ? collection.get(target) : collection.getName(target);
-                if (!document) {
-                    return getBrokenElement('Entity not Found');
-                }
-                if (!datapath) {
-                    return getBrokenElement('No datapath given "#<Type>[<id>]{datapath|templatename}"');
-                }
+    replaceTextContent(textNodes, documentRegexp, (_, type, target, datapath): HTMLElement => {
+        let document: Document | null = null;
+        if (CONST.ENTITY_TYPES.includes(type)) {
+            // Get the linked Entity
+            const collectionName = CONFIG[type].documentClass.collectionName;
+            const collection = game[collectionName];
+            document = /^[a-zA-Z0-9]{16}$/.test(target) ? collection.get(target) : collection.getName(target);
+            if (!document) {
+                return getBrokenElement("Entity not Found");
             }
-            if (document) {
-                return resolveDocumentDisplay(document, type, datapath);
+            if (!datapath) {
+                return getBrokenElement('No datapath given "#<Type>[<id>]{datapath|templatename}"');
             }
-            return getBrokenElement('document');
         }
-    );
+        if (document) {
+            return resolveDocumentDisplay(document, type, datapath);
+        }
+        return getBrokenElement("document");
+    });
 };
 
 /**
  * method to dispatch the differnet render functions
- * 
- * @param foundryDocument 
- * @param documentType 
- * @param datapath 
+ *
+ * @param foundryDocument
+ * @param documentType
+ * @param datapath
  */
 const resolveDocumentDisplay = (foundryDocument: Document, documentType, datapath: string): HTMLElement => {
     if (datapath.startsWith('"') && datapath.endsWith('"')) {
@@ -65,10 +166,10 @@ const resolveDocumentDisplay = (foundryDocument: Document, documentType, datapat
 
 /**
  * render a singular data path
- * 
- * @param foundryDocument 
- * @param documentType 
- * @param datapath 
+ *
+ * @param foundryDocument
+ * @param documentType
+ * @param datapath
  */
 const resolveDataPath = (foundryDocument, documentType, datapath: string): HTMLElement => {
     // Update link data
@@ -86,7 +187,7 @@ const resolveDataPath = (foundryDocument, documentType, datapath: string): HTMLE
         if (current && current[paths[i]]) {
             current = current[paths[i]];
         } else {
-            return getBrokenElement('Datapath broken');
+            return getBrokenElement("Datapath broken");
         }
     }
 
@@ -100,16 +201,16 @@ const resolveDataPath = (foundryDocument, documentType, datapath: string): HTMLE
 
 /**
  * render a template
- * 
- * @param foundryDocument 
- * @param templateName 
+ *
+ * @param foundryDocument
+ * @param templateName
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const resolveNamedTemplate = (foundryDocument: StoredDocument<any>, templateName: string): HTMLElement => {
     templateName = templateName.slice(1, -1);
 
-    if (!(game instanceof Game)){
-        return getBrokenElement('Game not Found');
+    if (!(game instanceof Game)) {
+        return getBrokenElement("Game not Found");
     }
 
     const templateFolder = game.folders?.find((folder) => {
@@ -125,12 +226,12 @@ const resolveNamedTemplate = (foundryDocument: StoredDocument<any>, templateName
     });
 
     if (!templateFile) {
-        return getBrokenElement('Template not Found');
+        return getBrokenElement("Template not Found");
     }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const templateScript = Handlebars.compile((templateFile.data as any).content);
-    return htmlToElement(templateScript({documentName: foundryDocument.documentName, id: foundryDocument.id , data: foundryDocument.data}));//, 
+    return htmlToElement(templateScript({ documentName: foundryDocument.documentName, id: foundryDocument.id, data: foundryDocument.data })); //,
 };
 
 const applyDataset = (element, data): void => {
@@ -139,15 +240,15 @@ const applyDataset = (element, data): void => {
             element.dataset[k] = v;
         }
     }
-}
+};
 
 const getBrokenElement = (msg) => {
     const a = document.createElement("a");
-    a.classList.add('entity-link');
+    a.classList.add("entity-link");
     a.draggable = true;
     a.innerHTML = `<i class="fas fa-unlink broken"></i> Broken (${msg})`;
     return a;
-}
+};
 
 /**
  * function to apply all the renderJournalSheet hooks
@@ -159,5 +260,6 @@ const getBrokenElement = (msg) => {
 export const renderJournalSheet = (_app: JournalSheet, html: JQuery<HTMLElement>, _data) => {
     console.log("Womboras Journal Renderer Executed");
     renderTableToChatButton(html);
+    renderRollRequests(html);
     renderDocumentData(html);
 };
